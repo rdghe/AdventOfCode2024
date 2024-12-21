89e36bc5aa4d1e0f9012b53267d48f356b80869d
Radu Gheorghe <radu.gheorghe@snyk.io>
2024-12-21 19:28:03 +0200

    feat: day 2 part 1

--- internal/day2/day2.go
+++ internal/day2/day2.go
@@ -3,6 +3,8 @@ package day2
 import (
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func Solve(part2 bool, example bool) error {
@@ -36,13 +38,67 @@ func Solve(part2 bool, example bool) error {
 }
 
 func solvePartOne(inputString string) error {
-	log.Println("solving day 1 part 1")
+	log.Println("solving day 2 part 1")
+	lines := strings.Split(inputString, "\n")
+
+	safeReports := 0
+
+	for _, line := range lines {
+		var numbers []int
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
+		}
+
+		for _, i := range fields {
+			j, err := strconv.Atoi(i)
+			if err != nil {
+				panic(err)
+			}
+			numbers = append(numbers, j)
+		}
+		log.Println(numbers)
+
+		isSafe := customIsIncreasing(numbers) || customIsDecreasing(numbers)
+		if isSafe {
+			log.Printf("is safe (increasing: %v, decreasing: %v)", customIsIncreasing(numbers), customIsDecreasing(numbers))
+			safeReports++
+		} else {
+			log.Printf("is unsafe")
+		}
+	}
+
+	log.Printf("the result is: %d\n", safeReports)
 
 	return nil
 }
 
+func customIsIncreasing(arr []int) bool {
+	for i := 1; i < len(arr); i++ {
+		current := arr[i]
+		previous := arr[i-1]
+		difference := current - previous
+		if current <= previous || difference > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func customIsDecreasing(arr []int) bool {
+	for i := 1; i < len(arr); i++ {
+		current := arr[i]
+		previous := arr[i-1]
+		difference := current - previous
+		if current >= previous || difference < -3 {
+			return false
+		}
+	}
+	return true
+}
+
 func solvePartTwo(inputString string) error {
-	log.Println("solving day 1 part 2")
+	log.Println("solving day 2 part 2")
 
 	return nil
 }
